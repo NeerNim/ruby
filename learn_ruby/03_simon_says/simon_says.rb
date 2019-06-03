8a2db6ea2f74d691eff4911c0747c04bf35ac7c5
NeerNim <neernimt@gmail.com>
2019-06-03 23:02:28 +0200

    make tests pass for simon says

--- learn_ruby/03_simon_says/simon_says.rb
+++ learn_ruby/03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
 #write your code here
+def echo(defualt = "hello")
+  defualt
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, defualt = 2)
+  say_word = ""
+  defualt.times { say_word += word}  
+  say_word
+end
+
+def start_of_word(word, num)
+  str = word.slice(0, num)
+  str  
+end
+
+def first_word(words) 
+  split_str = words.split(" ")
+  split_str[0]
+end
+
+
+def titleize(word)
+  small_words = %w[over the and]
+  
+  word.split(" ").map.with_index do |letter, i|  
+    unless (small_words.include? letter) and (i > 0 )
+      letter.capitalize
+    else
+      letter
+    end
+  end.join(" ")
+
+end
+
